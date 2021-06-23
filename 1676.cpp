72d59bede930acc68e22ab21f3d16b75d2d50fe0
narin111 <jungnr11@naver.com>
2021-06-23 15:50:48 +0900

    06231

---
+++ 1676.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+// 팩토리얼 0의 개수
+// 0의 2개인 이유는 소인수분해로 알 수 있다.
+// 항상 2의 개수가 많다.
+// 핵심: 5의 개수가 몇개인가
+// 100을 5로 나눈다 -> 20개
+// 100을 5*5로 나눈다 -> 4개
+  
+int main(){
+	int num;
+	int sum=0;
+	cin >> num;
+	for(int i=5; i<num+1; i*=5){
+		sum+=(num/i);
+	}
+
+	cout << sum;
+	return 0;
+}
